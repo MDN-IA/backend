56aa96ad7c788b12a0ebb5bad73c584f5d14df7d
870421 <870421@unizar.es>
2025-11-13 18:18:09 +0100

    changes

--- src/routes/samples.routes.js
+++ src/routes/samples.routes.js
@@ -1,5 +1,7 @@
-const router = require('express').Router();
+const { Router } = require('express');
 const { registerSample } = require('../controllers/samples.controller');
+
+const router = Router();
 
 router.post('/:roomCode', registerSample);
 module.exports = router;
